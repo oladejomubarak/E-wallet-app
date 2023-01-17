014db41898f3522dc54493c46c9bb036abaad549
oladejomubarak <oladejomubarakade@gmail.com>
2023-01-17 04:15:01 +0000

    more commit

--- src/main/java/oladejo/mubarak/unicoin/user/UserServicesImpl.java
+++ src/main/java/oladejo/mubarak/unicoin/user/UserServicesImpl.java
@@ -1,12 +1,16 @@
 package oladejo.mubarak.unicoin.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServicesImpl implements UserServices{
+    @Autowired
+    private UserRepository userRepository;
     @Override
     public String createAccount(User user) {
-        return "registered";
+        userRepository.save(user);
+        return "created";
 
     }
 }
