55b872cac59c63b9f7de40af2ba4b428889cc34b
oladejomubarak <oladejomubarakade@gmail.com>
2023-01-22 11:19:59 +0000

    change password controller completed

--- src/main/java/oladejo/mubarak/unicoin/registration/dtos/LoginRequest.java
+++ src/main/java/oladejo/mubarak/unicoin/registration/dtos/LoginRequest.java
@@ -1,9 +1,12 @@
 package oladejo.mubarak.unicoin.registration.dtos;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class LoginRequest {
+    @NotNull
     private String emailAddress;
+    @NotNull
     private String password;
 }
--- src/main/java/oladejo/mubarak/unicoin/registration/dtos/RegistrationRequest.java
+++ src/main/java/oladejo/mubarak/unicoin/registration/dtos/RegistrationRequest.java
@@ -1,5 +1,6 @@
 package oladejo.mubarak.unicoin.registration.dtos;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
@@ -8,9 +9,12 @@ import lombok.Setter;
 @Data
 @AllArgsConstructor
 public class RegistrationRequest {
-
+    @NotNull
     private String firstName;
+    @NotNull
     private String lastName;
+    @NotNull
     private String emailAddress;
+    @NotNull
     private String password;
 }
