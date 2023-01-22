b9f2489cfd970385e105d478386708edbfc0cd1f
oladejomubarak <oladejomubarakade@gmail.com>
2023-01-22 01:03:36 +0000

    change password controller implemented

--- src/main/java/oladejo/mubarak/unicoin/user/UserController.java
+++ src/main/java/oladejo/mubarak/unicoin/user/UserController.java
@@ -1,12 +1,34 @@
 package oladejo.mubarak.unicoin.user;
 
+import jakarta.mail.MessagingException;
+import jakarta.servlet.http.HttpServletRequest;
+import oladejo.mubarak.unicoin.user.dtos.ChangePasswordRequest;
+import oladejo.mubarak.unicoin.utils.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.ZonedDateTime;
 
 @RestController
 @RequestMapping(path = "api/v1/user")
 public class UserController {
     @Autowired
     private UserServices userServices;
+    @PatchMapping("/changepassword")
+    public ResponseEntity<?> changePassword(@RequestBody ChangePasswordRequest changePasswordRequest,
+                                            HttpServletRequest httpServletRequest) throws MessagingException {
+        ApiResponse apiResponse = ApiResponse.builder()
+                .statusCode(HttpStatus.OK.value())
+                .data(userServices.changePassword(changePasswordRequest))
+                .timeStamp(ZonedDateTime.now())
+                .path(httpServletRequest.getRequestURI())
+                .isSuccessful(true)
+                .build();
+        return new ResponseEntity<ApiResponse>(apiResponse, HttpStatus.OK);
+    }
 }
