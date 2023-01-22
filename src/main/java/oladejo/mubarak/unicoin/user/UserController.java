52f56c192034da84638dee105211f2db27760203
oladejomubarak <oladejomubarakade@gmail.com>
2023-01-22 00:54:28 +0000

    change password controller implemented

---
+++ src/main/java/oladejo/mubarak/unicoin/user/UserController.java
@@ -0,0 +1,12 @@
+package oladejo.mubarak.unicoin.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/user")
+public class UserController {
+    @Autowired
+    private UserServices userServices;
+}
