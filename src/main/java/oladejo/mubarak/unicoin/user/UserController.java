7439a8d4b77901e5b2e8f439dcc260d9b44c372e
oladejomubarak <oladejomubarakade@gmail.com>
2023-01-24 00:43:19 +0000

    delete user controller edited

--- src/main/java/oladejo/mubarak/unicoin/user/UserController.java
+++ src/main/java/oladejo/mubarak/unicoin/user/UserController.java
@@ -7,10 +7,7 @@ import oladejo.mubarak.unicoin.utils.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.ZonedDateTime;
 
@@ -19,6 +16,18 @@ import java.time.ZonedDateTime;
 public class UserController {
     @Autowired
     private UserServices userServices;
+    @PatchMapping("/changepassword")
+    public ResponseEntity<?> changePassword(@RequestBody ChangePasswordRequest changePasswordRequest,
+                                            HttpServletRequest httpServletRequest) throws MessagingException {
+        ApiResponse apiResponse = ApiResponse.builder()
+                .statusCode(HttpStatus.OK.value())
+                .data(userServices.changePassword(changePasswordRequest))
+                .timeStamp(ZonedDateTime.now())
+                .path(httpServletRequest.getRequestURI())
+                .isSuccessful(true)
+                .build();
+        return new ResponseEntity<ApiResponse>(apiResponse, HttpStatus.OK);
+    }
     @PatchMapping("/changepassword")
     public ResponseEntity<?> changePassword(@RequestBody ChangePasswordRequest changePasswordRequest,
                                             HttpServletRequest httpServletRequest) throws MessagingException {
